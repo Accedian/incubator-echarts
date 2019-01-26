d48c55757a60ef4a329f3c119e112d01822892cc
Steven Glanzer <sglanzer@accedian.com>
2019-01-25 21:32:46 -0500

    fix(parallel): Set the category axis map for all category axis dims

--- src/model/referHelper.js
+++ src/model/referHelper.js
@@ -161,9 +161,11 @@ var fetchers = {
             var axisDim = coordSysDims[index];
             axisMap.set(axisDim, axisModel);
 
-            if (isCategory(axisModel) && result.firstCategoryDimIndex == null) {
+            if (isCategory(axisModel)) {
                 categoryAxisMap.set(axisDim, axisModel);
-                result.firstCategoryDimIndex = index;
+                if (result.firstCategoryDimIndex == null) {
+                    result.firstCategoryDimIndex = index;
+                }
             }
         });
     }
